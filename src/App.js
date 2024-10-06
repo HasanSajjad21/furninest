c774211a6de27534dd95b0c0380b49852b76afe4
HasanSajjad21 <zahidsajjad2121@gmail.com>
2024-10-06 12:54:10 +0600

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Features from './components/Features';
+import Products from './components/Products';
+import Testimonials from './components/Testimonials';
+import Footer from './components/Footer';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Hero />
+      <Features />
+      <Products />
+      <Testimonials />
+      <Footer />
     </div>
   );
 }
