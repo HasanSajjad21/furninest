ddf818184609365917e3f2b7b81c6c65b7a060cf
HasanSajjad21 <zahidsajjad2121@gmail.com>
2024-10-06 12:45:58 +0600

    new project

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+// src/components/Navbar.js
+import React from 'react';
+import { Link } from 'react-scroll';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">FurniNest</div>
+      <ul className="nav-links">
+        <li><Link to="home" smooth={true}>Home</Link></li>
+        <li><Link to="products" smooth={true}>Products</Link></li>
+        <li><Link to="blog" smooth={true}>Blog</Link></li>
+        <li><Link to="contact" smooth={true}>Contact</Link></li>
+      </ul>
+      <div className="auth-buttons">
+        <button className="btn">Sign In</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
